ae868c759d2ec29ccfaa620020225f03360bf2d5
Wilmer S Zuna Largo <zunalargowilmer@gmail.com>
2021-08-12 09:57:33 -0700

    Added MainPlayer & Script BoilerPlate

---
+++ Assets/Scripts/GameScene/MainPlayerScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MainPlayerScript : MonoBehaviour {
+  public int LIVES = 3;
+  // void Update() {
+    
+  // }
+}
